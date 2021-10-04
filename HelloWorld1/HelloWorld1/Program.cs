5b606a835473afdcb55a2692e21849ffc7915439
fishyaoC <89052660+fishyaoC@users.noreply.github.com>
2021-10-04 10:56:26 +0800

    Add files via upload
    try_catch_finally_practise
    2數字相除

---
+++ HelloWorld1/HelloWorld1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace HelloWorld1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // try-catch-finally
+
+            int numerator, denominator;
+
+            try
+            {
+                Console.Write("Please enter numerator");
+                numerator = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Please enter denominator");
+                denominator = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("The result is {0}", numerator / denominator);
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("Error: You did not enter an integer.");
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("--- End of Error Handling Example ---");
+            }
+
+
+        }
+    }
+}
